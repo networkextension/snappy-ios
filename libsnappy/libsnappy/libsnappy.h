d50b6220e29a038555945af0fc0b4e72872566bb
abigt <abigt@gmail.com>
2017-04-28 22:07:26 +0800

    add Framework

---
+++ libsnappy/libsnappy/libsnappy.h
@@ -0,0 +1,20 @@
+//
+//  libsnappy.h
+//  libsnappy
+//
+//  Created by 孔祥波 on 28/04/2017.
+//  Copyright © 2017 Kong XiangBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libsnappy.
+FOUNDATION_EXPORT double libsnappyVersionNumber;
+
+//! Project version string for libsnappy.
+FOUNDATION_EXPORT const unsigned char libsnappyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+#import <libsnappy/snappy-c.h>
+
+
