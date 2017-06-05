a2f1ff465d4af2deb3663dbb9544806177c5db39
yarshure <yarshure@gmail.com>
2017-06-05 11:54:42 +0800

    support macOS

---
+++ libsnappy/snappyOSX/snappy.h
@@ -0,0 +1,19 @@
+//
+//  snappyOSX.h
+//  snappyOSX
+//
+//  Created by yarshure on 2017/6/5.
+//  Copyright © 2017年 Kong XiangBo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for snappyOSX.
+FOUNDATION_EXPORT double snappyOSXVersionNumber;
+
+//! Project version string for snappyOSX.
+FOUNDATION_EXPORT const unsigned char snappyOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <snappyOSX/PublicHeader.h>
+
+#import <snappy/snappy-c.h>
